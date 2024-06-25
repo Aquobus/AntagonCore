3315fb7d1089999abc0f2af86b1254c667efd2f3
Aquobus <asanbekisaev9@gmail.com>
2024-06-25 22:13:42 +0300

    First commit

--- src/main/java/com/aquobus/antagoncore/AntagonCore.java
+++ src/main/java/com/aquobus/antagoncore/AntagonCore.java
@@ -1,17 +1,55 @@
 package com.aquobus.antagoncore;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
 
-public final class AntagonCore extends JavaPlugin {
+import org.kingdoms.constants.group.Kingdom;
+import org.kingdoms.constants.player.KingdomPlayer;
+import org.kingdoms.events.general.GroupDisband;
+import org.kingdoms.events.general.KingdomCreateEvent;
+
+import java.util.Objects;
+
+public final class AntagonCore extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onKingdomCreate(KingdomCreateEvent event) {
+        Kingdom kingdom = event.getKingdom();
+        KingdomPlayer king = Objects.requireNonNull(event.getKingdom()).getKing();
+        Player player = king.getPlayer();
+
+        assert player != null;
+        player.sendMessage("Ваше величество, поздравляю с основанием королевства! Но учтите, что если в королевстве не будет минимум 3 человека через 12 часов после его создания, то оно будет уничтожено");
+
+        BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+        scheduler.scheduleSyncDelayedTask((Plugin) this, new Runnable() {
+            @Override
+            public void run() {
+                assert kingdom != null;
+                if (kingdom.getKingdomPlayers().size() < 3) {
+                    Objects.requireNonNull(kingdom.getGroup()).disband(GroupDisband.Reason.CUSTOM);
+
+                    if (player.isOnline()) {
+                        player.sendMessage("Ваше королевство было уничтожено по причине недостаточного количества людей");
+                    }
+                }
+            }
+        }, 200L);
     }
 }
