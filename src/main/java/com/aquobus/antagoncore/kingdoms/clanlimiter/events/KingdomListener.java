9e9242bd1aad5937cfb8758f36468d6cc692f15b
Aquobus <asanbekisaev9@gmail.com>
2024-06-26 01:26:07 +0300

    added config.yml and some changes in KingdomListener.java

--- src/main/java/com/aquobus/antagoncore/kingdoms/clanlimiter/events/KingdomListener.java
+++ src/main/java/com/aquobus/antagoncore/kingdoms/clanlimiter/events/KingdomListener.java
@@ -5,7 +5,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitScheduler;
 import org.kingdoms.constants.group.Kingdom;
 import org.kingdoms.constants.player.KingdomPlayer;
@@ -16,6 +15,8 @@ import org.kingdoms.events.members.KingdomLeaveEvent;
 import java.util.Objects;
 
 public class KingdomListener implements Listener {
+
+    public BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
     @EventHandler
     public void onKingdomCreate(KingdomCreateEvent event) {
         Kingdom kingdom = event.getKingdom();
@@ -24,28 +25,24 @@ public class KingdomListener implements Listener {
 
         assert player != null;
         player.sendMessage("Ваше величество, поздравляю с основанием королевства! Но учтите, что если в королевстве не будет минимум 3 человека через 12 часов после его создания, то оно будет уничтожено");
-
-        BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
         scheduler.scheduleSyncDelayedTask(AntagonCore.getPlugin(), new Runnable() {
             @Override
             public void run() {
                 assert kingdom != null;
-                if (kingdom.getKingdomPlayers().size() < 3) {
+                if (kingdom.getKingdomPlayers().size() < AntagonCore.getPlugin().disbandPlayerMinimum) {
                     Objects.requireNonNull(kingdom.getGroup()).disband(GroupDisband.Reason.CUSTOM);
 
                     if (player.isOnline()) {
-                        player.sendMessage("Ваше королевство было уничтожено по причине недостаточного количества людей");
+                        player.sendMessage("Ваше королевство было уничтожено из-за недостаточного количества игроков.");
                     }
                 }
             }
-        }, 200L);
+        }, AntagonCore.getPlugin().disbandDelayHoursAfterLeavedPlayer * 3600 * 20L);
     }
 
     @EventHandler
     public void onKingdomLeave(KingdomLeaveEvent event) {
         Kingdom kingdom = event.getKingdom();
-
-        BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
         scheduler.scheduleSyncDelayedTask(AntagonCore.getPlugin(), new Runnable() {
             @Override
             public void run() {
@@ -56,9 +53,9 @@ public class KingdomListener implements Listener {
                             player.sendMessage("Ваше королевство будет уничтожено из-за недостаточного количества игроков.");
                         }
                     }
-                    kingdom.getGroup().disband(GroupDisband.Reason.CUSTOM);
+                    Objects.requireNonNull(kingdom.getGroup()).disband(GroupDisband.Reason.CUSTOM);
                 }
             }
-        }, AntagonCore.getPlugin().disbandDelayHoursAfterLeavedPlayer * 60 * 60 * 20L); // Задержка в часах, конвертирующаяся в тики
+        }, AntagonCore.getPlugin().disbandDelayHoursAfterLeavedPlayer * 3600 * 20L); // Задержка в часах, конвертирующаяся в тики
     }
 }
