2ef83f225cd3323e994666ad18e12f57e1535ab7
Aquobus <asanbekisaev9@gmail.com>
2024-06-26 16:51:47 +0300

    modified:   src/main/java/com/aquobus/antagoncore/AntagonCore.java

--- src/main/java/com/aquobus/antagoncore/AntagonCore.java
+++ src/main/java/com/aquobus/antagoncore/AntagonCore.java
@@ -3,6 +3,7 @@ package com.aquobus.antagoncore;
 import com.aquobus.antagoncore.commands.ACore;
 import com.aquobus.antagoncore.commands.CommandCompleter;
 import com.aquobus.antagoncore.kingdoms.clanlimiter.events.KingdomListener;
+import com.aquobus.antagoncore.kingdoms.ultimaaddon.events.ElytraListener;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -22,12 +23,15 @@ public final class AntagonCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getServer().getLogger().info("AntagonCore успешно был включен");
+
         // Plugin startup logic
         saveDefaultConfig();
 
         // Events register
-        getServer().getLogger().info("AntagonCore успешно был включен");
+        getServer().getPluginManager().registerEvents(new ElytraListener(this), this);
         getServer().getPluginManager().registerEvents(new KingdomListener(this), this);
+        
 
         // Commands register
         getServer().getPluginCommand("antagoncore").setExecutor(new ACore(this));
